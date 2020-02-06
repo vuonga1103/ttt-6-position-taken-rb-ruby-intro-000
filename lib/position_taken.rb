72eb3989dac18e3f1168659558b393473a4e170a
Anh Vuong <vuonga1103@yahoo.com>
2020-02-06 21:00:02 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,5 +1,5 @@
 def position_taken?(board, index)
-  if board[index] == " " || board[index] == ""
+  if board[index] == " " || board[index] == "" || board[index] == nil
     return false
   end
 end
